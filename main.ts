93185fe524e4376664ad53057ae9063bfc9500b4
Jet-Lu888 <jet.lu@stu.ocsb.ca>
2026-02-17 13:21:29 -0500

    more formatting

--- main.ts
+++ main.ts
@@ -9,10 +9,6 @@ basic.pause(1000)
 //dimensions
 basic.showString('A rectangle has dimensions 5 cm & 3 cm.')
 //perimeter 
-basic.showString("The perimeter: 2(5+3)="  + (2 * (5+3)) + "cm")
+basic.showString("The perimeter: 2(5+3)="  + (2 * (5+3)) + " cm")
 //area
-basic.showString("The area: 5x3 =" + (5 * 3) + "cm^2")
-
-
-
-
+basic.showString("The area: 5x3 =" + (5 * 3) + " cm^2")
