e54c6d733f2ab6f8cc3e1edfe9cffc37d74c8e92
Jet-Lu888 <jet.lu@stu.ocsb.ca>
2026-02-17 21:14:29 +0000

    Created a python program that runs baisc calculations and created a hex for that program.

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,9 +1,18 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
-This module is a Micro:bit MicroPython program
+Created by: Jet Lu
+Created on: Feb 2026
+This module is a Micro:bit MicroPython program that can perform basic math
 """
 
 from microbit import *
 
-display.scroll("Hello, World!")
+sleep(1000)
+
+# Dimensions
+display.scroll("A rectangle has dimensions 5 cm & 3 cm")
+
+# Perimeter
+display.scroll("The perimeter: 2(5+3)=" + (2 * (5 + 3)) + " cm")
+
+# Area
+display.scroll("The area: 5x3 =" + (5 * 3) + " cm^2")
