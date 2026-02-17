45f99ada9550192c226ce6bf175a24f3f5bb58c0
Jet-Lu888 <jet.lu@stu.ocsb.ca>
2026-02-17 13:05:47 -0500

    Programmed basic calculator and changed read me.

--- main.ts
+++ main.ts
@@ -1,8 +1,18 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Jet Lu
+ * Created on: Feb 2026
+ * This program will calculate area and perimeter
 */
 
-basic.showString('Hello, World!')
+basic.pause(1000)
+//dimensions
+basic.showString('A rectangle has dimensions 5 cm & 3 cm.')
+//perimeter 
+basic.showString("The perimeter: 2(5+3)="  + (2*(5+3)) + "cm")
+//area
+basic.showString("The area: 5x3 =" + (5 * 3) + "cm^2")
+
+
+
+
