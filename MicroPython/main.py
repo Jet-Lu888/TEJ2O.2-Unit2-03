38cc1f1c74b93900ad96bfcad75d47c5082f9a4b
Jet-Lu888 <jet.lu@stu.ocsb.ca>
2026-02-19 17:24:48 +0000

    Reformated comments

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,18 +1,18 @@
 """
 Created by: Jet Lu
 Created on: Feb 2026
-This module is a Micro:bit MicroPython program that can perform basic math
+This module is a Micro:bit MicroPython program that can perform basic math.
 """
 
 from microbit import *
 
 sleep(1000)
 
-# Dimensions
+# dimensions
 display.scroll("A rectangle has dimensions 5 cm & 3 cm")
 
-# Perimeter
+# perimeter
 display.scroll("The perimeter: 2(5+3)=" + (2 * (5 + 3)) + " cm")
 
-# Area
+# area
 display.scroll("The area: 5x3 =" + (5 * 3) + " cm^2")
